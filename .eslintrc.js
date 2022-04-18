355d1479631038271541c67c2a4fb05df75b7672
Samk13 <samarbid13@gmail.com>
2022-04-18 02:02:04 +0200

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "plugin:react/recommended",
+    "airbnb",
+    "eslint:recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+      module: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["react", "react-hooks"],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    semi: ["error", "never"],
+    quotes: ["error", "double"],
+    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
+  },
+}
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from "react"
+import { render } from "react-dom"
+import HiComponent from "./components/Hi"
+
+render(<HiComponent />, document.getElementById("app"))
